45a90ff38e60f6123337a5b358963bdadc5dabf5
SuperKinetic <yugandhardandekar@gmail.com>
2025-03-02 12:15:44 +0000

    implement solve by step

--- test.py
+++ test.py
@@ -12,9 +12,7 @@ matrix = [[Fraction(x) for x in row] for row in matrix]
 
 s = Solver(matrix, variables=['x', 'y', 'z'], slack_variables=['s1', 's2', 's3'])
 
-print(s.dumps() + '\n')
-
-s.solve()
-
-print(s.dumps() + '\n')
+for _ in s.solve_by_step():
+    print(s.dumps() + '\n')
+    
 print(s.get_solution())
